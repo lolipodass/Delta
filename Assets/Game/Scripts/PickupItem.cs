fb6f6fed39988b328e1d777f2e9569bce3d065ae
andremae10@gmail.com <>
2025-05-18 23:13:41 +0300

    add item shake

--- Assets/Game/Scripts/PickupItem.cs
+++ Assets/Game/Scripts/PickupItem.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using PrimeTween;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,8 +7,7 @@ public class PickupItem : MonoBehaviour
 {
     [field: SerializeField] private UpgradeItemData Data;
 
-
-    private void Start()
+    private void Awake()
     {
         if (Data == null)
         {
@@ -16,13 +17,16 @@ public class PickupItem : MonoBehaviour
         }
         var image = GetComponent<SpriteRenderer>();
         image.sprite = Data.Icon;
+        Tween.ShakeLocalPosition(transform, duration: 1f, strength: new Vector3(0, 0.1f, 0), cycles: -1);
+        // Tween.PositionY(transform, end0.01f, duration: 0.5f, cycles: -1, cycleMode: CycleMode.Yoyo);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             InventoryManager.Instance.AddItem(Data);
-            Destroy(gameObject);
+            Tween.Scale(transform, 1, 0f, duration: 0.5f).OnComplete(target: transform, ui => Destroy(gameObject));
         }
     }
     private void OnValidate()
